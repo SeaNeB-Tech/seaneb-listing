270f3afb8c42c21642f3868083f21c7916402fd2
Samad Saiyed <125883326+samad-scs@users.noreply.github.com>
2025-04-03 17:24:10 +0530

    fix: remove unused routes from constants

--- src/constants/sitemap-routes.ts
+++ src/constants/sitemap-routes.ts
@@ -1,11 +1,5 @@
 export const sitemapRoutes = {
   // ** SITE
   home: '/',
-  aboutUs: '/about-us',
-  services: '/services',
-  technologies: '/technologies',
-  contactUs: '/contact-us',
-  privacyPolicy: '/privacy-policy',
-  refundPolicy: '/refund-policy',
-  termsAndConditions: '/terms-and-conditions'
+   
 }
