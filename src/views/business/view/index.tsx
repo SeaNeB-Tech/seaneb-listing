df4324ae9ff85696287c26d76efdec4a8798d837
Samad Saiyed <125883326+samad-scs@users.noreply.github.com>
2025-04-08 16:22:17 +0530

    refactor: adjust minimum height in BusinessDetails component for improved layout consistency

--- src/views/business/view/index.tsx
+++ src/views/business/view/index.tsx
@@ -32,7 +32,7 @@ const BusinessDetails = ({ businessData, testimonials }: BusinessDetailsProps) =
   return (
     <ScreenWrapper className='grid grid-cols-1 gap-x-10 gap-y-10 py-10 lg:grid-cols-12 lg:py-20'>
       {/* Left Side View */}
-      <div className='lg:col-span-8'>
+      <div className='lg:col-span-8 lg:min-h-[50vh]'>
         <h2 className='text-3xl font-medium'>{businessData?.business_name}</h2>
         <div className='mt-2 flex items-center gap-2'>
           <MapPin className='h-4 w-4 text-gray-500' />
